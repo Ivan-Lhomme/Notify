a13542fc8a4b8e1f602a4aab63ae5bae06f6d7d0
Ivan <i.lhommeageorges@gmail.com>
2026-05-16 12:14:18 +0200

    Move delete and modify user  to reop/utils.go

--- internal/repository/admin.go
+++ internal/repository/admin.go
@@ -40,19 +40,3 @@ func Add_user(db *sql.DB, new_user models.User) error {
 
 	return err
 }
-
-func Delete_user(db *sql.DB, UUID string) error {
-	query := `DELETE FROM users WHERE id=$1`
-
-	_, err := db.Exec(query, UUID)
-
-	return err
-}
-
-func Modify_user(db *sql.DB, new_user_data models.User) error {
-	query := `UPDATE users SET pseudo=$1, email=$2, passwd=$3, id_role=$4 WHERE id=$5`
-
-	_, err := db.Exec(query, new_user_data.Pseudo, new_user_data.Email, new_user_data.Password, new_user_data.Role, new_user_data.UUID)
-
-	return err
-}
