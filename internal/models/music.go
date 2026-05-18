4030fe17bb707b854cc0bdcbc2d97789ddfaeebe
Ivan <i.lhommeageorges@gmail.com>
2026-05-18 10:20:02 +0200

    Fix not align json field

--- internal/models/music.go
+++ internal/models/music.go
@@ -6,7 +6,7 @@ type Music struct {
 	UUID         string 	`json:"uuid"`
 	Id_publisher string 	`json:"id_publisher"`
 	Title        string 	`json:"title"`
-	Explicit     bool 	`json:"explicit"`
+	Explicit     bool 		`json:"explicit"`
 	Plays_count  int    	`json:"plays_count"`
 	Duration     int    	`json:"duration"`
 	Bitrate      int    	`json:"bitrate"`
