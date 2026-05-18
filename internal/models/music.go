17bbf9aaef9494a13b2278ec4ed2db680fddc8f3
Ivan <i.lhommeageorges@gmail.com>
2026-05-18 10:27:38 +0200

    Add add_play_count() to music struct

--- internal/models/music.go
+++ internal/models/music.go
@@ -18,3 +18,7 @@ func (music *Music) Modify_music(new_music_data Music) {
 	if new_music_data.Title != "" { music.Title = new_music_data.Title }
 	music.Explicit = new_music_data.Explicit
 }
+
+func (music *Music) Add_play_count(nbr int) {
+	music.Plays_count = music.Plays_count + nbr
+}
