9fe09fca607b1f28f5497b5255d196cec2aadd0d
Ivan <i.lhommeageorges@gmail.com>
2026-05-27 10:48:48 +0200

    Frontend - Complete the profile page

--- Notify-frontend/src/pages/Profile.tsx
+++ Notify-frontend/src/pages/Profile.tsx
@@ -1,3 +1,47 @@
+import { useState } from "react";
+import apiFetch from "../utils/apiFetch";
+
+type data = {
+  start: boolean;
+  user: any;
+};
+
 export default function Profile() {
-  return <></>;
+  const [data, setData]: [data, Function] = useState({
+    start: true,
+    user: undefined,
+  });
+
+  const logout = async () => {
+    await apiFetch("/logout", "GET");
+    window.location.reload();
+  };
+  const requestData = async () => {
+    const response = await apiFetch("/api/user/profile", "GET");
+
+    setData({
+      start: false,
+      user: (await response.json()).data,
+    });
+  };
+
+  if (data.start) requestData();
+
+  if (data.user === undefined) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <h1>{data.user.pseudo}</h1>
+      <h2>{data.user.email}</h2>
+      <p>
+        {data.user.role === 1
+          ? "Admin"
+          : data.user.user === 2
+            ? "Artist"
+            : "User"}
+      </p>
+
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
 }
