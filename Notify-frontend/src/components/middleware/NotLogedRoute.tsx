a64656ee4fd27832b3d3c0ca383243b8d87c0aef
Ivan <i.lhommeageorges@gmail.com>
2026-05-28 11:26:37 +0200

    Frontend - Move middlewares into a folder

--- Notify-frontend/src/components/middleware/NotLogedRoute.tsx
+++ Notify-frontend/src/components/middleware/NotLogedRoute.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, type ReactNode } from "react";
 import { Navigate } from "react-router";
-import apiFetch from "../utils/apiFetch";
+import apiFetch from "../../utils/apiFetch";
 
 export default function NotLogedRoute({ children }: { children: ReactNode }) {
   const [isAuth, setIsAuth]: [Boolean | null, Function] = useState(null);
@@ -9,7 +9,7 @@ export default function NotLogedRoute({ children }: { children: ReactNode }) {
     apiFetch("/refresh", "GET").finally(() => {
       apiFetch(`/auth`, "GET").then((res) => setIsAuth(res.status === 403));
     });
-  });
+  }, []);
 
   if (isAuth === null) return <p>Loading...</p>;
   if (isAuth) return <Navigate to="/" />;
--- Notify-frontend/src/components/middleware/ProtectedRoute.tsx
+++ Notify-frontend/src/components/middleware/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, type ReactNode } from "react";
 import { Navigate } from "react-router";
-import apiFetch from "../utils/apiFetch";
+import apiFetch from "../../utils/apiFetch";
 
 export default function ProtectedRoute({
   reqAuth,
