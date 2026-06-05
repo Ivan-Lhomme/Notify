7b0a8c7699fc44b57f04aa99cf6488cb2d3552f1
Ivan <i.lhommeageorges@gmail.com>
2026-06-06 00:07:50 +0200

    Frontend - Add an anti-spam upload and add the accept type file in the input

--- Notify-frontend/src/pages/Upload.tsx
+++ Notify-frontend/src/pages/Upload.tsx
@@ -4,23 +4,28 @@ import styles from "../assets/css/upload.module.css";
 import { useState } from "react";
 
 export default function Upload() {
+  const [loading, setLoading] = useState(false);
   const [errMessage, setErrorMessage] = useState("");
 
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const formData = new FormData(e.target);
+    if (!loading) {
+      setLoading(true);
+      const formData = new FormData(e.target);
 
-    const res = await apiFetch("/api/artist/uploadmusic", "POST", formData);
+      const res = await apiFetch("/api/artist/uploadmusic", "POST", formData);
 
-    if (!res.ok) {
-      const body = await res.json();
-      if (body.message) setErrorMessage(body.message);
+      if (!res.ok) {
+        const body = await res.json();
+        if (body.message) setErrorMessage(body.message);
 
-      return;
+        setLoading(false);
+        return;
+      }
+
+      window.location.href = "/";
     }
-
-    window.location.href = "/";
   };
 
   return (
@@ -35,8 +40,8 @@ export default function Upload() {
       <label htmlFor="explicit">Is explicit</label>
       <input type="checkbox" name="explicit" id="explicit" />
 
-      <label htmlFor="music">Music (in .ogg please)</label>
-      <input type="file" name="music" id="music" />
+      <label htmlFor="music">Music (in .ogg only)</label>
+      <input type="file" name="music" id="music" accept=".ogg" />
 
       <button type="submit">Upload</button>
       <button onClick={() => (window.location.href = "/")}>Cancel</button>
